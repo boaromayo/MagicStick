a75e8017ae2805ee3bf209197b0d99a1746ce22e
Nico Poblete <nlypoblet@gmail.com>
2017-07-18 23:39:18 -0700

    Renamed methods
    Refactored move() to update() and draw() to render()

--- src/base/Entity.java
+++ src/base/Entity.java
@@ -4,8 +4,8 @@ import java.awt.*;
 
 public interface Entity {
 
-	public void move();
-	public void draw(Graphics g);
+	public void update();
+	public void render(Graphics g);
 	public Graphics2D antialias(Graphics g);
 	
 	public void setDX(float dx);
