3801354ec9b4a9d875f2db31f07b4833c728cdb9
Nico Poblete <nlypoblet@gmail.com>
2017-07-29 20:18:38 -0700

    Uploaded scene files

---
+++ src/scene/SceneMenu.java
@@ -0,0 +1,18 @@
+package scene;
+
+import java.awt.Graphics;
+
+public class SceneMenu extends Scene {
+	// CONSTRUCTOR.
+	public SceneMenu() {
+		
+	}
+	
+	public void updateGame() {
+		
+	}
+	
+	public void renderGame(Graphics g) {
+		
+	}
+}
