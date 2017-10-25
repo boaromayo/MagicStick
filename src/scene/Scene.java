b82e8d72d29ad51957f9b2cc12b29c37ddf0f3d2
Nico Poblete <nlypoblet@gmail.com>
2017-10-24 19:00:50 -0700

    Commented out delay()

--- src/scene/Scene.java
+++ src/scene/Scene.java
@@ -8,7 +8,7 @@ public abstract class Scene {
 	public abstract void updateGame();
 	public abstract void renderGame(Graphics g);
 	
-	public void delay(int s) {
+	/*public void delay(int s) {
 		int frames = 0;
 		int count = 0;
 		
@@ -20,5 +20,5 @@ public abstract class Scene {
 				System.out.println(count);
 			}
 		}
-	}
+	}*/
 }
